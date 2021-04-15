5631c8e312a51eac96968cb1c7ab4d443700b9a8
Josh Kessler <Josh.H.Kessler@gmail.com>
2021-04-14 21:07:43 -0400

    Adds JSON schema validation

--- rating.py
+++ rating.py
@@ -1,12 +1,34 @@
 import flask
 from flask import jsonify, request
+from flask_expects_json import expects_json
 
 from utils import handle_json_data
 
 app = flask.Flask(__name__)
 
+schema = {
+    "type": "object",
+    "properties": {
+            "CustomerID": {"type": "integer"},
+        "DwellingCoverage": {"type": "integer", "minimum": 0},
+        "HomeAge": {"type": "integer", "minimum": 0},
+        "RoofType": {
+            "type": "string",
+            "pattern": "^(Asphalt Shingles|Tin|Wood)$"},
+        "NumberOfUnits": {"type": "integer", "minimum": 1, "maximum": 4},
+        "PartnerDiscount": {"type": "string", "pattern": "^(Y|N)$"}
+    },
+    "required": [
+        "CustomerID",
+        "DwellingCoverage",
+        "HomeAge",
+        "RoofType",
+        "NumberOfUnits"]
+}
+
 
 @app.route("/", methods=["POST"])
+@expects_json(schema)
 def rating():
     data = request.get_json()
     value = handle_json_data(data)
